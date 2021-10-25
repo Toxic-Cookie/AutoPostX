53528ed7571f471cda9a830afcaabc905ab75f44
Toxic_Cookie <66691163+Toxic-Cookie@users.noreply.github.com>
2021-10-24 17:17:34 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostX;
+using System.Threading;
+
+namespace AutoPostX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Attempting to process assembly at path: " + args[0]);
+
+            try
+            {
+                NeosAssemblyPostProcessor.Process(args[0]);
+
+                Console.WriteLine("Successfully processed assembly!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception running post processing!");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+
+                Console.WriteLine("Exiting in 5 seconds...");
+
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AutoPostX
+ A tiny utility to streamline plugin testing.
